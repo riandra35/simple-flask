57167e1a738cda296bade6f3ec3335860f16adbd
riandra35 <108457598+riandra35@users.noreply.github.com>
2026-05-24 08:23:15 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,8 +5,8 @@ import os
 app = Flask(__name__)
 
 # Konfigurasi Supabase
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
+url = os.environ.get("https://pnjygyusrduossxouvav.supabase.co")
+key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuanlneXVzcmR1b3NzeG91dmF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTQzMDI2MCwiZXhwIjoyMDk1MDA2MjYwfQ.3wV66UBat1J_SPdbpuUlNIbPeb9lPwk1ragXkuftIKg")
 supabase: Client = create_client(url, key)
 
 @app.route('/api/tasks', methods=['GET', 'POST', 'PUT', 'DELETE'])
